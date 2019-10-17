98bbd1d4ddc0391b2e9e5d02bc1870557ed4e0d9
JayZang <sa841218as@gmail.com>
2019-10-17 10:21:19 +0800

    impelment API for getting a member base info

--- backend/src/api/routes/member.ts
+++ backend/src/api/routes/member.ts
@@ -8,38 +8,48 @@ const router = Router()
 const memberService = Container.get(MemberService)
 
 export default (app: Router) => {
-  app.use('/members', router)
+    app.use('/members', router)
 
-  router.post('', CreateMemberValidator(), async (req: Request, res: Response) => {
-    const member = await memberService.create(req.body)
+    router.post('', CreateMemberValidator(), async (req: Request, res: Response) => {
+        const member = await memberService.create(req.body)
 
-    return member ?
-      res.json(member) :
-      res.status(400).end()
-  })
+        return member ?
+            res.json(member) :
+            res.status(400).end()
+    })
 
-  router.get('', async (req, res) => {
-    const members = await memberService.all()
+    router.get('', async (req, res) => {
+        const members = await memberService.all()
 
-    return res.json(members)
-  })
+        return res.json(members)
+    })
 
-  router.patch('/:id', UpdateMemberValidator(), async (req: Request, res: Response) => {
-    const member = await memberService.update(
-      req.params.id,
-      req.body
-    )
+    router.patch('/:id', UpdateMemberValidator(), async (req: Request, res: Response) => {
+        const member = await memberService.update(
+            req.params.id,
+            req.body
+        )
 
-    return member ? 
-      res.json(member) :
-      res.status(400).end()
-  })
+        return member ?
+            res.json(member) :
+            res.status(400).end()
+    })
 
-  router.delete('/:id', async (req, res) => {
-    const member = await memberService.delete(req.params.id)
+    router.delete('/:id', async (req, res) => {
+        const member = await memberService.delete(req.params.id)
 
-    return member ?
-      res.json(member) :
-      res.status(400).end() 
-  })
+        return member ?
+            res.json(member) :
+            res.status(400).end()
+    })
+
+    router.get('/:id', async (req, res) => {
+        const member = await memberService.getBaseInfoById(
+            req.params.id
+        )
+
+        return member ?
+            res.json(member) :
+            res.status(400).end()
+    })
 }
--- backend/src/services/MemberService.ts
+++ backend/src/services/MemberService.ts
@@ -6,60 +6,76 @@ import MemberRepo from '@/repository/MemberRepository'
 
 @Service()
 export default class MemberService {
-  
-  /**
-   * Create One Member
-   */
-  public async create(data: any) {
-    try {
-      const memberRepo = getCustomRepository(MemberRepo)
-      return await memberRepo.createAndSave(data)
-    } catch (err) {
-      console.log('Create Member fail')
-      console.log(err.toString())
-      return null
-    }
-  }
 
-  /**
-   * Get All Members
-   */
-  public async all() {
-    const memberRepo = getCustomRepository(MemberRepo)
-    return memberRepo.find()
-  }
-
-  /**
-   * Update One Member
-   * 
-   * @param id       Member id
-   * @param data  field to update
-   */
-  public async update(id: string | number, data: any) {
-    try {
-      const memberRepo = getCustomRepository(MemberRepo)
-      return await memberRepo.updateById(id, data)
-    } catch (err) {
-      console.log('Update member fail')
-      console.log(err.toString())
-      return null
+    /**
+     * Create One Member
+     */
+    public async create(data: any) {
+        try {
+            const memberRepo = getCustomRepository(MemberRepo)
+            return await memberRepo.createAndSave(data)
+        } catch (err) {
+            console.log('Create Member fail')
+            console.log(err.toString())
+            return null
+        }
     }
-  }
 
-  /**
-   * Delete One Member
-   * 
-   * @param id    Member id
-   */
-  public async delete(id: string | number) {
-    try {
-      const memberRepo = getCustomRepository(MemberRepo)
-      const member =  await memberRepo.findOneOrFail(id)      
-      return await memberRepo.remove(member)
-    } catch (err) {
-      console.log('Delete member fail')
-      console.log(err.toString())
-      return null
+    /**
+     * Get All Members
+     */
+    public async all() {
+        const memberRepo = getCustomRepository(MemberRepo)
+        return memberRepo.find()
     }
-  }
+
+    /**
+     * Update One Member
+     * 
+     * @param id       Member id
+     * @param data  field to update
+     */
+    public async update(id: string | number, data: any) {
+        try {
+            const memberRepo = getCustomRepository(MemberRepo)
+            return await memberRepo.updateById(id, data)
+        } catch (err) {
+            console.log('Update member fail')
+            console.log(err.toString())
+            return null
+        }
+    }
+
+    /**
+     * Delete One Member
+     * 
+     * @param id    Member id
+     */
+    public async delete(id: string | number) {
+        try {
+            const memberRepo = getCustomRepository(MemberRepo)
+            const member = await memberRepo.findOneOrFail(id)
+            return await memberRepo.remove(member)
+        } catch (err) {
+            console.log('Delete member fail')
+            console.log(err.toString())
+            return null
+        }
+    }
+
+    /**
+     * Get Member Base Info 
+     * 
+     * @param id    Member id
+     */
+    public async getBaseInfoById(id: string | number) {
+        try {
+            const memberRepo = getCustomRepository(MemberRepo)
+            return await memberRepo.findOneOrFail(id)
+        } catch (err) {
+            console.log('Find member fail')
+            console.log(err.toString())
+            return null
+        }
+    }
 }
