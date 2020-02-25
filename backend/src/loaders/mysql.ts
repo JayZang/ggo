39e9fdde409f2ee5f6f9a3940be545f0ed46b7c8
JayZang <sa841218as@gmail.com>
2020-02-25 15:00:13 +0800

    setup typeorm config environment (.env file)

--- backend/src/loaders/mysql.ts
+++ backend/src/loaders/mysql.ts
@@ -1,28 +1,11 @@
-import { createConnection } from 'typeorm'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
 import { database } from '@/config'
 
 const mysqlConfig = database.mysql
 
 export default async () => {
-    const connectionPromise = createConnection({
-        type: "mysql",
-        host: mysqlConfig.host,
-        port: 3306,
-        username: mysqlConfig.username,
-        password: mysqlConfig.password,
-        database: mysqlConfig.database,
-        entities: [
-            `${__dirname}/../entity/*.js`
-        ],
-        synchronize: true,
-        migrationsTableName: "migrations",
-        migrations: ["migration/*.js"],
-        cli: {
-            migrationsDir: "migration"
-        },
-        logging: false
-    })
+    const connectionPromise = createConnection()
   
     await connectionPromise.then(connection => {
         console.log('MySQL Connect Success')
