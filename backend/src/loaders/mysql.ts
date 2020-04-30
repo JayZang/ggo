ba8eb42a5b894c006f608d7aff6a4eadbe7f8df2
JayZang <jay.mailspace@gmail.com>
2020-04-30 22:50:46 +0800

    fix user's task work report edition bug. update db charset=utf8mb4_unicode_ci

--- backend/src/loaders/mysql.ts
+++ backend/src/loaders/mysql.ts
@@ -5,7 +5,12 @@ import { database } from '@/config'
 const mysqlConfig = database.mysql
 
 export default async () => {
-    const connectionPromise = createConnection()
+    const connectionOptions = await getConnectionOptions();
+    if (connectionOptions.type === 'mysql')
+        // supported to insert emoji character
+        Object.assign(connectionOptions, {charset: 'utf8mb4_unicode_ci'})
+
+    const connectionPromise = createConnection(connectionOptions)
   
     await connectionPromise.then(connection => {
         console.log('MySQL Connect Success')
--- frontend/src/components/WorkReport/WorkReportPanel/WorkReportList/index.tsx
+++ frontend/src/components/WorkReport/WorkReportPanel/WorkReportList/index.tsx
@@ -5,7 +5,10 @@ import WorkReportList from './WorkReportList'
 import { UserIdentityType } from 'contracts/user';
 
 const mapStateToProps = (state: RootState) => ({
-    userMemberId: state.auth.user && state.auth.user.identity_type === UserIdentityType.member ?
+    userMemberId: state.auth.user && [
+        UserIdentityType.manager, 
+        UserIdentityType.member
+    ].includes(state.auth.user.identity_type) ?
         state.auth.user.identity_id :
         null
 })
